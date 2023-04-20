6d68725afef16d8ef70400bbae611725e05bcd24
Yes <ppozniak10@gmail.com>
2023-04-20 16:51:31 +0200

    added sql insert categories to sqlcommands.sql

--- SqlCommands.sql
+++ SqlCommands.sql
@@ -1,1 +1,12 @@
+INSERT INTO categories (category_name)
+VALUES 
+('toys'),
+('digital services'),
+('cosmetics and body care'),
+('food and beverage'),
+('health and wellness'),
+('household items'),
+('media'),
+('pet care'),
+('office equipment');
 
